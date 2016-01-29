72e002eb16bbc6eb143ce1caed5eb52498cef04f
Craig McNamara <cmcnamara87@gmail.com>
2016-01-30 09:00:37 +1000

    change google analytics property for best in town

--- resources/views/includes/analyticstracking.blade.php
+++ resources/views/includes/analyticstracking.blade.php
@@ -4,7 +4,7 @@
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-    ga('create', 'UA-30890335-1', 'auto');
+    ga('create', 'UA-51312192-8', 'auto');
     ga('send', 'pageview');
 
 </script>

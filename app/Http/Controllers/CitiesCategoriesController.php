fa24b661db42a6ff58bfbe982f59547fc84fded2
Craig McNamara <cmcnamara87@gmail.com>
2016-02-09 21:59:49 +1000

    refactor(rank list): hide ranks == 0

--- app/Http/Controllers/CitiesCategoriesController.php
+++ app/Http/Controllers/CitiesCategoriesController.php
@@ -30,8 +30,9 @@ class CitiesCategoriesController extends Controller
         $ranks = Rank::where('category_id', '=', $category->id)
             ->where('city_id', $city->id)
             ->where('rank', '>', 0)
-            ->with('place', 'place.ranks', 'place.ranks.category')
-            ->get();
+            ->with(['place', 'place.ranks' => function($query) use ($city) {
+                $query->where('rank', '>', 0);
+            }, 'place.ranks.category'])->get();
 
         return view('categories.show', compact('city', 'category', 'ranks', 'categories'));
     }
